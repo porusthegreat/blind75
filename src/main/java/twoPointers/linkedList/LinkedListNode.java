dc6505c3325069745dfca71d36989fbb3f834a00
porusthegreat <reddys.purushotham@gmail.com>
2023-11-07 13:36:48 +0000

    added product except self and removing nth node in LL

---
+++ src/main/java/twoPointers/linkedList/LinkedListNode.java
@@ -0,0 +1,11 @@
+package twoPointers.linkedList;
+
+public class LinkedListNode {
+    int data;
+    LinkedListNode next;
+
+    public LinkedListNode(int data) {
+        this.data = data;
+        this.next = null;
+    }
+}
